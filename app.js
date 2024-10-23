2285274a4d0e61f33cb45e81b6aeee391417ffb8
Aran Hiblot <a.hiblot@student.isartdigital.com>
2024-10-23 20:10:53 +0200

    project finished

--- app.js
+++ app.js
@@ -42,9 +42,21 @@ app.get("/classrooms", function (req, res) {
 });
 
 app.get("/classrooms/:classroom_id/students", function (req, res) {
+    let classroom_id = req.params.classroom_id;
+
+    if (
+        classroom_id == null ||
+        classroom_id == "" ||
+        !Number.isInteger(parseInt(classroom_id, 10))
+    ) {
+        return res.status(422).json({ error: "The classroom is not valid" });
+    }
+
+    classroom_id = parseInt(classroom_id, 10);
+
     connection.query(
-        "SELECT * FROM students WHERE classroom_id = (?)",
-        [req.params.classroom_id],
+        "SELECT * FROM classrooms WHERE id = (?)",
+        [classroom_id],
         (error, results) => {
             if (error) {
                 console.error(
@@ -55,7 +67,29 @@ app.get("/classrooms/:classroom_id/students", function (req, res) {
                     .json({ error: "Failed to fetch classes" });
             }
 
-            return res.status(200).send(results);
+            if (results.length < 1) {
+                return res
+                    .status(404)
+                    .json({ message: "This classroom does not exist" });
+            }
+
+            connection.query(
+                "SELECT * FROM students WHERE classroom_id = (?)",
+                [classroom_id],
+                (error, results) => {
+                    if (error) {
+                        console.error(
+                            "Error fetching classes from the database: " +
+                                error.stack
+                        );
+                        return res
+                            .status(500)
+                            .json({ error: "Failed to fetch classes" });
+                    }
+
+                    return res.status(200).send(results);
+                }
+            );
         }
     );
 });
@@ -112,15 +146,27 @@ app.post("/classrooms", function (req, res) {
 
 app.post("/classrooms/:classroom_id/students", function (req, res) {
     const student = req.body.name;
-    const classroom = req.params.classroom_id;
+    let classroom = req.params.classroom_id;
 
     if (student == null || student == "") {
         return res.status(422).json({ error: "There is no student name" });
     }
 
+    if (
+        classroom == null ||
+        classroom == "" ||
+        !Number.isInteger(parseInt(classroom, 10))
+    ) {
+        return res
+            .status(422)
+            .json({ message: "Classroom ID is not an integer" });
+    }
+
+    classroom = parseInt(classroom, 10);
+
     connection.query(
-        "SELECT * FROM classrooms WHERE name = (?)",
-        [classroom],
+        "SELECT * FROM students WHERE name = (?) AND classroom_id = (?)",
+        [student, classroom],
         (error, results) => {
             if (error) {
                 console.error(
@@ -132,14 +178,14 @@ app.post("/classrooms/:classroom_id/students", function (req, res) {
             }
 
             if (results.length > 0) {
-                return res
-                    .status(409)
-                    .json({ error: "This classroom already exists" });
+                return res.status(409).json({
+                    error: "This student already exists in this classroom",
+                });
             }
 
             connection.query(
-                `INSERT INTO classrooms (name) VALUES (?)`,
-                [classroom],
+                `INSERT INTO students (classroom_id, name) VALUES (?, ?)`,
+                [classroom, student],
                 (error, results) => {
                     if (error) {
                         console.error(
@@ -152,9 +198,189 @@ app.post("/classrooms/:classroom_id/students", function (req, res) {
                     }
 
                     return res.status(201).json({
-                        message: "Classroom added successfully",
+                        message: "Student added successfully",
                         results,
                     });
+                }
+            );
+        }
+    );
+});
+
+app.put("/classrooms/:id", (req, res) => {
+    let classroom_id = req.params.id;
+    let classroom_name = req.body.name;
+
+    if (classroom_name == null || classroom_name == "") {
+        return res.status(422).json({ error: "There is no classroom name" });
+    }
+
+    if (
+        classroom_id == null ||
+        classroom_id == "" ||
+        !Number.isInteger(parseInt(classroom_id, 10))
+    ) {
+        return res
+            .status(422)
+            .json({ message: "Classroom ID is not an integer" });
+    }
+
+    classroom_id = parseInt(classroom_id, 10);
+
+    connection.query(
+        "SELECT * FROM classrooms WHERE id = (?)",
+        [classroom_id],
+        (error, results) => {
+            if (error) {
+                console.error(
+                    "Error fetching classes from the database: " + error.stack
+                );
+                return res
+                    .status(500)
+                    .json({ error: "Failed to fetch classes" });
+            }
+
+            if (results.length < 1) {
+                return res
+                    .status(404)
+                    .json({ message: "This classroom does not exist" });
+            }
+
+            connection.query(
+                "SELECT * FROM classrooms WHERE name = (?)",
+                [classroom_name],
+                (error, results) => {
+                    if (error) {
+                        console.error(
+                            "Error fetching classes from the database: " +
+                                error.stack
+                        );
+                        return res
+                            .status(500)
+                            .json({ error: "Failed to fetch classes" });
+                    }
+
+                    if (results.length > 0) {
+                        return res.status(409).json({
+                            message: "Another classroom already has this name",
+                        });
+                    }
+
+                    connection.query(
+                        "UPDATE classrooms SET name = (?) WHERE id = (?)",
+                        [classroom_name, classroom_id],
+                        (error, results) => {
+                            if (error) {
+                                console.error(
+                                    "Error fetching classes from the database: " +
+                                        error.stack
+                                );
+                                return res
+                                    .status(500)
+                                    .json({ error: "Failed to fetch classes" });
+                            }
+
+                            return res.status(200).json({
+                                message: "Classroom successfully updated",
+                            });
+                        }
+                    );
+                }
+            );
+        }
+    );
+});
+
+app.put("/classrooms/:classroom_id/students/:student_id", (req, res) => {
+    let classroom_id = req.params.classroom_id;
+    let student_id = req.params.student_id;
+    let student_name = req.body.name;
+
+    if (student_name == null || student_name == "") {
+        return res.status(422).json({ error: "There is no student name" });
+    }
+
+    if (
+        classroom_id == null ||
+        classroom_id == "" ||
+        !Number.isInteger(parseInt(classroom_id, 10))
+    ) {
+        return res
+            .status(422)
+            .json({ message: "Classroom ID is not an integer" });
+    }
+    classroom_id = parseInt(classroom_id, 10);
+
+    if (
+        student_id == null ||
+        student_id == "" ||
+        !Number.isInteger(parseInt(student_id, 10))
+    ) {
+        return res
+            .status(422)
+            .json({ message: "Student ID is not an integer" });
+    }
+    student_id = parseInt(student_id, 10);
+
+    connection.query(
+        "SELECT * FROM classrooms WHERE id = (?)",
+        [classroom_id],
+        (error, results) => {
+            if (error) {
+                console.error(
+                    "Error fetching classes from the database: " + error.stack
+                );
+                return res
+                    .status(500)
+                    .json({ error: "Failed to fetch classes" });
+            }
+
+            if (results.length < 1) {
+                return res
+                    .status(404)
+                    .json({ message: "This classroom does not exist" });
+            }
+
+            connection.query(
+                "SELECT * FROM students WHERE name = (?) AND classroom_id = (?)",
+                [student_name, classroom_id],
+                (error, results) => {
+                    if (error) {
+                        console.error(
+                            "Error fetching classes from the database: " +
+                                error.stack
+                        );
+                        return res
+                            .status(500)
+                            .json({ error: "Failed to fetch classes" });
+                    }
+
+                    if (results.length > 0) {
+                        return res.status(409).json({
+                            message:
+                                "Another student already has this name in this classroom",
+                        });
+                    }
+
+                    connection.query(
+                        "UPDATE students SET name = (?) WHERE id = (?) AND classroom_id = (?)",
+                        [student_name, student_id, classroom_id],
+                        (error, results) => {
+                            if (error) {
+                                console.error(
+                                    "Error fetching classes from the database: " +
+                                        error.stack
+                                );
+                                return res
+                                    .status(500)
+                                    .json({ error: "Failed to fetch classes" });
+                            }
+
+                            return res.status(200).json({
+                                message: "Student successfully updated",
+                            });
+                        }
+                    );
                 }
             );
         }
@@ -169,25 +395,33 @@ app.delete("/classrooms/:id", (req, res) => {
         classroomId == "" ||
         !Number.isInteger(parseInt(classroomId, 10))
     ) {
-        return res.status(422).json({ message: "ID is not an integer" });
+        return res
+            .status(422)
+            .json({ message: "Classroom ID is not an integer" });
     }
 
     classroomId = parseInt(classroomId, 10);
 
-    connection.query("SELECT * FROM classrooms", (error, results) => {
-        if (error) {
-            console.error(
-                "Error fetching classes from the database: " + error.stack
-            );
-            return res.status(500).json({ error: "Failed to fetch classes" });
-        }
+    connection.query(
+        "SELECT * FROM classrooms WHERE id = (?)",
+        [classroomId],
+        (error, results) => {
+            if (error) {
+                console.error(
+                    "Error fetching classes from the database: " + error.stack
+                );
+                return res
+                    .status(500)
+                    .json({ error: "Failed to fetch classes" });
+            }
 
-        if (!results.some((element) => element.id == classroomId)) {
-            return res
-                .status(404)
-                .json({ error: "This classroom does not exist" });
+            if (results.length < 1) {
+                return res
+                    .status(404)
+                    .json({ error: "This classroom does not exist" });
+            }
         }
-    });
+    );
 
     connection.query(
         `DELETE FROM classrooms WHERE id = (?)`,
@@ -205,6 +439,72 @@ app.delete("/classrooms/:id", (req, res) => {
             return res
                 .status(201)
                 .json({ message: "Classroom deleted successfully" });
+        }
+    );
+});
+
+app.delete("/classrooms/:classroom_id/students/:student_id", (req, res) => {
+    let classroomId = req.params.classroom_id;
+    let student_id = req.params.student_id;
+
+    if (
+        classroomId == null ||
+        classroomId == "" ||
+        !Number.isInteger(parseInt(classroomId, 10))
+    ) {
+        return res
+            .status(422)
+            .json({ message: "This classroom does not exist" });
+    }
+
+    if (
+        student_id == null ||
+        student_id == "" ||
+        !Number.isInteger(parseInt(student_id, 10))
+    ) {
+        return res.status(422).json({ message: "This student does not exist" });
+    }
+
+    classroomId = parseInt(classroomId, 10);
+    student_id = parseInt(student_id, 10);
+
+    connection.query(
+        "SELECT * FROM students WHERE classroom_id = (?) AND id = (?)",
+        [classroomId, student_id],
+        (error, results) => {
+            if (error) {
+                console.error(
+                    "Error fetching classes from the database: " + error.stack
+                );
+                return res
+                    .status(500)
+                    .json({ error: "Failed to fetch classes" });
+            }
+
+            if (results.length < 1) {
+                return res.status(404).json({
+                    error: "This student does not exist in this classroom",
+                });
+            }
+        }
+    );
+
+    connection.query(
+        `DELETE FROM students WHERE id = (?)`,
+        [student_id],
+        (error) => {
+            if (error) {
+                console.error(
+                    "Error deleting class from the database: " + error.stack
+                );
+                return res
+                    .status(500)
+                    .json({ error: "Failed to delete student" });
+            }
+
+            return res
+                .status(201)
+                .json({ message: "Student deleted successfully" });
         }
     );
 });
